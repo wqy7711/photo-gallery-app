819947e80fce2fe3e03e3fd1770477d02ae6e9fc
unknown <944325009@qq.com>
2025-04-28 04:56:02 +0100

    Implemented SNS topic and SQS queues

--- lib/photo-gallery-app-stack.ts
+++ lib/photo-gallery-app-stack.ts
@@ -2,11 +2,12 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns from 'aws-cdk-lib/aws-sns';
 
 export class PhotoGalleryAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     const photoBucket = new s3.Bucket(this, 'PhotoBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
@@ -30,6 +31,24 @@ export class PhotoGalleryAppStack extends cdk.Stack {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    
+    const imageTopic = new sns.Topic(this, 'ImageTopic', {
+      displayName: 'Image Events Topic'
+    });
+    
+    const imageDeadLetterQueue = new sqs.Queue(this, 'ImageDeadLetterQueue', {
+      queueName: 'ImageDeadLetterQueue',
+      retentionPeriod: cdk.Duration.days(14)
+    });
+    
+    const imageUploadQueue = new sqs.Queue(this, 'ImageUploadQueue', {
+      queueName: 'ImageUploadQueue',
+      visibilityTimeout: cdk.Duration.seconds(30),
+      deadLetterQueue: {
+        queue: imageDeadLetterQueue,
+        maxReceiveCount: 3
+      }
     });
   }
 }
